cfe4283daff4c3cc3d736b69f3fa53ba27b1a8c6
harsh <harshvaish2003@gmail.com>
2025-07-02 17:06:48 +0530

    modified telescope keymaps

--- lua/mecodes/lazy/telescope.lua
+++ lua/mecodes/lazy/telescope.lua
@@ -14,12 +14,7 @@ return {
 					},
 				},
 			},
-			pickers = {
-				find_files = {
-					file_ignore_patterns = { "node_modules", ".git", ".venv" },
-					hidden = true,
-				},
-			},
+						
 			live_grep = {
 				file_ignore_patterns = { "node_modules", ".git", ".venv" },
 				additional_args = function(_)
@@ -29,16 +24,42 @@ return {
 		})
 
 		local builtin = require("telescope.builtin")
-
+	
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+
+		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[S]earch Git [F]iles" })
+
+		-- from everyfile, even in node_modules
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+
+		-- vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+
+		-- vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+
+		-- to grap a word
+		vim.keymap.set('n', '<leader>pw', function()
+            		local word = vim.fn.expand("<cword>")
+            		builtin.grep_string({ search = word })
+        	end)
+
+		-- to grap a string (the string until whitespace)
+		vim.keymap.set('n', '<leader>ps', function()
+           		local word = vim.fn.expand("<cWORD>")
+            		builtin.grep_string({ search = word })
+        	end)
+
+		-- vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+		-- vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		-- vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+		
+		vim.keymap.set('n', '<leader>ss', function()
+            		builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        	end)
+
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
 		vim.keymap.set("n", "<leader>so", function()
