b61619c17e036cede2ccb2b1a2095df0ad6dfbb0
harsh <harshvaish2003@gmail.com>
2025-07-02 23:37:47 +0530

    chores and lsp setup

--- lua/mecodes/lazy/telescope.lua
+++ lua/mecodes/lazy/telescope.lua
@@ -1,72 +1,89 @@
-return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.8',
-	branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-        	require('telescope').setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
-						["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
-						["<C-l>"] = require("telescope.actions").select_default, -- open file
+return  {
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+		branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			require('telescope').setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+							["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
+							["<C-l>"] = require("telescope.actions").select_default, -- open file
+						},
 					},
 				},
-			},
-						
-			live_grep = {
-				file_ignore_patterns = { "node_modules", ".git", ".venv" },
-				additional_args = function(_)
-					return { "--hidden" }
-				end,
-			},
-		})
 
-		local builtin = require("telescope.builtin")
-	
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-
-		vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[S]earch Git [F]iles" })
-
-		-- from everyfile, even in node_modules
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-
-		-- vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-
-		-- vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-
-		-- to grap a word
-		vim.keymap.set('n', '<leader>pw', function()
-            		local word = vim.fn.expand("<cword>")
-            		builtin.grep_string({ search = word })
-        	end)
-
-		-- to grap a string (the string until whitespace)
-		vim.keymap.set('n', '<leader>ps', function()
-           		local word = vim.fn.expand("<cWORD>")
-            		builtin.grep_string({ search = word })
-        	end)
-
-		-- vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		-- vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		-- vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		
-		vim.keymap.set('n', '<leader>ss', function()
-            		builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        	end)
-
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
-		vim.keymap.set("n", "<leader>so", function()
-			builtin.live_grep({
-				grep_open_files = true,
-				prompt_title = "Live Grep in Open Files",
+				live_grep = {
+					file_ignore_patterns = { "node_modules", ".git", ".venv" },
+					additional_args = function(_)
+						return { "--hidden" }
+					end,
+				},
 			})
-		end, { desc = "[S]earch [/] in Open Files" })
-	end,
+
+			local builtin = require("telescope.builtin")
+
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+
+			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+
+			vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[S]earch Git [F]iles" })
+
+			-- from everyfile, even in node_modules
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+
+			-- vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+
+			-- vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+
+			-- to grap a word
+			vim.keymap.set('n', '<leader>pw', function()
+				local word = vim.fn.expand("<cword>")
+				builtin.grep_string({ search = word })
+			end)
+
+			-- to grap a string (the string until whitespace)
+			vim.keymap.set('n', '<leader>ps', function()
+				local word = vim.fn.expand("<cWORD>")
+				builtin.grep_string({ search = word })
+			end)
+
+			-- vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			-- vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			-- vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+
+			vim.keymap.set('n', '<leader>ss', function()
+				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			end)
+
+			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+			vim.keymap.set("n", "<leader>so", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[S]earch [/] in Open Files" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function ()
+			require("telescope").setup {
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {
+						}
+
+					}
+				}
+			}
+			require("telescope").load_extension("ui-select")
+		end
+	}
 }
