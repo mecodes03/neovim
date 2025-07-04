8f94447e7402b45c4c626c66d2a33371a2480b30
harsh <harshvaish2003@gmail.com>
2025-07-04 21:04:49 +0530

    edited and added color schemes

--- lua/mecodes/lazy/colors.lua
+++ lua/mecodes/lazy/colors.lua
@@ -1,47 +1,70 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	local defaultColor = "rose-pine"
+	color = color or defaultColor
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 end
 
 return {
-    {
-        "folke/tokyonight.nvim",
+	{
+		"folke/tokyonight.nvim",
 		name = "tokyo-night",
-        config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-            })
-        end
-    },
+		config = function()
+			require("tokyonight").setup({
+				style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+				transparent = true, -- Enable this to disable setting the background color
+				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value for `:help nvim_set_hl`
+					comments = { italic = false },
+					keywords = { italic = false },
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "dark", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+				},
+			})
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				variant = "moon",
+				dark_variant = "moon",
+				dim_inactive_windows = true,
+				styles = {
+					bold = false,
+					italic = false,
+					transparent = true,
+				},
+				enable = { terminal = true },
+			})
 
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-            })
+			vim.cmd("colorscheme rose-pine")
 
-            vim.cmd("colorscheme rose-pine")
-
-            ColorMyPencils()
-        end
-    },
+			ColorMyPencils()
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = false,
+				term_colors = true,
+				no_italic = true,
+				no_bold = true,
+				integrations = {
+					cmp = true,
+					nvimtree = true,
+					treesitter = true,
+				},
+			})
+		end,
+	},
 }
