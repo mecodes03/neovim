1889f39e2fa373a3b7209262b68e4c965393ab1b
harsh <harshvaish2003@gmail.com>
2025-09-18 21:36:23 +0530

    added rust formatter

--- lua/mecodes/lazy/conform.lua
+++ lua/mecodes/lazy/conform.lua
@@ -4,6 +4,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				rust = { "rustfmt", lsp_format = "fallback" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
 				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
