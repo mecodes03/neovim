5c8ddae61593f02e47b2775b3219f757ba4922c5
harsh <harshvaish2003@gmail.com>
2025-09-20 04:49:48 +0530

    added some plugins

---
+++ lua/mecodes/lazy/misc.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		-- Detect tabstop and shiftwidth automatically
+		"tpope/vim-sleuth",
+	},
+	{
+		-- Hints keybinds
+		"folke/which-key.nvim",
+	},
+	{
+		-- Autoclose parentheses, brackets, quotes, etc.
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
+	{
+		-- Highlight todo, notes, etc in comments
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = true },
+	},
+}
