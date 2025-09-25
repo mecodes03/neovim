5e05b4fe1f14a643c40c3208d8959aae55edc2c4
harsh <harshvaish2003@gmail.com>
2025-09-25 22:15:36 +0530

    update: highlight colors

---
+++ lua/mecodes/lazy/highlight_colors.lua
@@ -0,0 +1,9 @@
+return {
+	"brenoprata10/nvim-highlight-colors",
+	event = "VeryLazy",
+	opts = {
+		render = "virtual", -- "foreground" / "background" / "virtual"
+		enable_named_colors = false,
+		enable_tailwind = true,
+	},
+}
