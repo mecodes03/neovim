15eb9e07a4f492af9c7d070a19b8abe6bf2598be
harsh <harshvaish2003@gmail.com>
2025-09-25 21:58:16 +0530

    feat: cool bufferline

--- lua/mecodes/lazy/bufferline.lua
+++ lua/mecodes/lazy/bufferline.lua
@@ -1,23 +1,50 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
+	"akinsho/bufferline.nvim", -- Bufferbar
+	dependencies = {
+		"rose-pine/neovim",
+	},
 	config = function()
-		vim.opt.termguicolors = true
 		local bufferline = require("bufferline")
-		require("bufferline").setup({
+		bufferline.setup({
 			options = {
-				style_preset = bufferline.style_preset.no_italic,
-				diagnostics = "nvim_lsp",
-				separator_style = "slope", -- or "thin" | "padded_slant" | "slant"
+				style_preset = {
+					bufferline.style_preset.no_italic,
+					bufferline.style_preset.no_bold,
+				},
+				numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+				close_command = "bdelete! %d",
+				right_mouse_command = "bdelete! %d",
+				left_mouse_command = "buffer %d",
+				middle_mouse_command = nil,
+				modified_icon = "●",
+				left_trunc_marker = "",
+				right_trunc_marker = "",
+				max_name_length = 30,
+				tab_size = 20,
+				diagnostics = false, -- false | "nvim_lsp" | "coc"
+				diagnostics_update_in_insert = false,
+				offsets = { { filetype = "NvimTree", text = "", padding = 2 } },
+				show_buffer_icons = true,
 				show_buffer_close_icons = false,
 				show_close_icon = false,
-				diagnostics_indicator = function(count, level)
-					local icon = level:match("error") and " " or " "
-					return " " .. icon .. count
-				end,
+				show_tab_indicators = false,
+				separator_style = { "", "" },
+				enforce_regular_tabs = true,
+				always_show_bufferline = true,
+				indicator = {
+					style = "none", -- "icon" | "underline" | "none"
+				},
 			},
-			highlights = require("rose-pine.plugins.bufferline"),
+			highlights = {
+				buffer_selected = {
+					bold = true,
+					italic = true,
+					fg = require("rose-pine.palette").rose,
+					-- bg = require("rose-pine.palette").rose,
+				},
+				tab = { fg = require("rose-pine.palette").rose },
+				buffer_visible = { fg = require("rose-pine.palette").rose },
+			},
 		})
 	end,
 }
