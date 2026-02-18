a2484dbd1ed24271e8d68f47e34f7abfcdaea980
mecodes03 <harshvaish2003@gmail.com>
2026-02-18 15:14:21 +0530

    update: moved smaller ones in misc file

--- lua/mecodes/lazy/misc.lua
+++ lua/mecodes/lazy/misc.lua
@@ -1,11 +1,48 @@
 return {
 	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end,
+	},
+
+	{
+		"brenoprata10/nvim-highlight-colors",
+		event = "VeryLazy",
+		opts = {
+			render = "virtual", -- "foreground" / "background" / "virtual"
+			enable_named_colors = false,
+			enable_tailwind = true,
+		},
+	},
+
+	{
 		"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	},
+
 	{
 		"folke/todo-comments.nvim", -- Highlight todo, notes, etc in comments
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = true },
 	},
+
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	}
 }
