c860eadd954de0934b195e32394606363dcdc0d8
mecodes03 <harshvaish2003@gmail.com>
2026-02-22 22:36:56 +0530

    fix: nvim dap keymaps

--- lua/mecodes/lazy/nvim-dap.lua
+++ lua/mecodes/lazy/nvim-dap.lua
@@ -21,12 +21,12 @@ return {
 		end
 
 		-- set a vim motion for <Space> + d + t to toggle a breakpoint at the line where the cursor is currently on
-		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "[D]ebug [T]oggle Breakpoint" })
+		vim.keymap.set("n", "<leader>Dt", dap.toggle_breakpoint, { desc = "[D]ebug [T]oggle Breakpoint" })
 
 		-- set a vim motion for <Space> + d + s to start the debugger and launch the debugging ui
-		vim.keymap.set("n", "<leader>ds", dap.continue, { desc = "[D]ebug [S]tart" })
+		vim.keymap.set("n", "<leader>Ds", dap.continue, { desc = "[D]ebug [S]tart" })
 
 		-- set a vim motion to close the debugging ui
-		vim.keymap.set("n", "<leader>dc", dapui.close, { desc = "[D]ebug [C]lose" })
+		vim.keymap.set("n", "<leader>Dc", dapui.close, { desc = "[D]ebug [C]lose" })
 	end
 }
