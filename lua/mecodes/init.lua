bb8141f425833ec36daf3463b668ed7b7a947ca1
Harsh Vaish <harshvaish2003@gmail.com>
2026-08-23 23:45:09 +0530

    fix

--- lua/mecodes/init.lua
+++ lua/mecodes/init.lua
@@ -169,7 +169,7 @@ autocmd("LspAttach", {
 
 		local function is_large(buf)
 			local max_filesize = 100 * 1024 -- 100 KB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
 			if ok and stats and stats.size > max_filesize then
 				return true
 			else
